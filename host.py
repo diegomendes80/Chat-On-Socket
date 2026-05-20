719d35d67264f777ff1b6a9763bf4937b82ac7a3
Diego Mendes <mendesdiego818@gmail.com>
2026-05-20 09:55:37 -0300

    code for host

---
+++ host.py
@@ -0,0 +1,63 @@
+import socket
+import threading
+
+HOST = '0.0.0.0'
+PORT = 12345
+
+def listen_mensage(conection):
+    while True:
+        try:
+            data = conection.recv(1024)
+            if not data:
+                print("\nConnection closed by the client.");
+                break
+           
+            mensage = data.decode('utf-8')
+            print(f"\nReceived message: {mensage}")
+        except Exception as e:
+            print(f"\nError receiving message: {e}")
+            break
+    
+    print("\nClosing connection.")
+    conection.close()
+    
+def send_mensage(conection):
+    print("\nConnection is ready to send messages.")
+    
+    while True:
+        try:
+            mensage = input("\nEnter a message to send (or 'exit' to quit): ")
+            
+            if not mensage.strip():
+                continue
+            
+            if mensage.lower() == 'exit':
+                break
+            
+            conection.send(mensage.encode('utf-8'))
+            
+        except Exception as e:
+            print(f"\nError sending message: {e}")
+            break
+
+    print("\nClosing connection.")
+    conection.close()
+    
+    
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    server.bind((HOST, PORT))
+    server.listen()
+    print("\n Server is listening on {}:{}".format(HOST, PORT))
+    
+    receiver_thread = threading.Thread(target=listen_mensage, args=(conection,), daemon=True)
+    sender_thread = threading.Thread(target=send_mensage, args=(conection,))
+    
+    receiver_thread.start()
+    sender_thread.start()
+    
+if __name__ == "__main__":
+    start_server()
